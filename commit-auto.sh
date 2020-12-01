1b1e405fc5f570b47079eff71ad8bdbb5caf78b7
davidfrickert <davidjfrickert@gmail.com>
2020-12-01 18:23:56 +0000

    Auto Commit (18:23_2020-12-01)

--- commit-auto.sh
+++ commit-auto.sh
@@ -1,2 +1,6 @@
+cd "/home/david/Thesis/FaaS-GraalVM"
+git fetch
+git pull
 git add *
-git commit -m "Auto Commit ($(printf '%(%H:%M_%Y-%m-%d)T\n' -1))" 
+git commit -m "Auto Commit ($(printf '%(%H:%M_%Y-%m-%d)T\n' -1))"
+git push
