1680a8f1d34644182f2002f2a736a0ce34694c81
David Frickert <david.frickert@protonmail.com>
2021-04-16 00:24:28 +0100

    fix metrics singleton initializer

--- src/main/java/pt/ist/photon_graal/metrics/MetricsSupport.java
+++ src/main/java/pt/ist/photon_graal/metrics/MetricsSupport.java
@@ -3,21 +3,25 @@ package pt.ist.photon_graal.metrics;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
-
-import java.util.Optional;
+import pt.ist.photon_graal.settings.CurrentSettings;
 
 public class MetricsSupport {
 
     private static MeterRegistry INSTANCE;
 
     public static MeterRegistry getMeterRegistry() {
-        return Optional
-                .ofNullable(INSTANCE)
-                .orElse(new PrometheusMeterRegistry(PrometheusConfig.DEFAULT));
+        if (INSTANCE == null)
+            INSTANCE = initRegistry(new PrometheusMeterRegistry(PrometheusConfig.DEFAULT));
+        return INSTANCE;
     }
 
     public static PrometheusMeterRegistry getMeterRegistryPrometheus() {
         return (PrometheusMeterRegistry) getMeterRegistry();
+    }
+
+    private static MeterRegistry initRegistry(MeterRegistry registry) {
+        registry.config().commonTags("function.name", CurrentSettings.VALUE.toString());
+        return registry;
     }
 
 }
