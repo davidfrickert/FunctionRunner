23ecdc2c68035ac0f6bc8d889cb1fb704f315440
dfrickert <david.frickert@premium-minds.com>
2021-05-02 14:18:33 +0100

    add more metrics

--- src/main/java/pt/ist/photon_graal/runner/FunctionRunnerImpl.java
+++ src/main/java/pt/ist/photon_graal/runner/FunctionRunnerImpl.java
@@ -1,6 +1,14 @@
 package pt.ist.photon_graal.runner;
 
 import io.vavr.control.Either;
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+import java.lang.reflect.Method;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.function.IntFunction;
+import java.util.stream.Collectors;
 import lombok.SneakyThrows;
 import org.apache.commons.lang3.SerializationUtils;
 import org.graalvm.nativeimage.CurrentIsolate;
@@ -11,16 +19,10 @@ import org.graalvm.nativeimage.c.function.CEntryPoint;
 import org.graalvm.nativeimage.c.function.CEntryPoint.IsolateThreadContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pt.ist.photon_graal.metrics.MetricsSupport;
 import pt.ist.photon_graal.runner.isolateutils.IsolateError;
 import pt.ist.photon_graal.runner.isolateutils.conversion.registry.TypeConversionRegistry;
 import pt.ist.photon_graal.runner.isolateutils.handles.HandleUnwrapUtils;
-
-import java.io.ByteArrayInputStream;
-import java.io.ObjectInputStream;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.function.IntFunction;
-import java.util.stream.Collectors;
 
 public class FunctionRunnerImpl implements FunctionRunner {
 
@@ -28,34 +30,54 @@ public class FunctionRunnerImpl implements FunctionRunner {
         return TypeConversionRegistry.getInstance();
     }
 
+    private static MetricsSupport ms;
+
+    static {
+        init();
+    }
+
+    @SneakyThrows
+    private static void init() {
+        ms = MetricsSupport.get();
+    }
+
     @Override
     @SneakyThrows
     public <T> Either<IsolateError, T> run(String className, String methodName, Object... args) {
-        IsolateThread currentIsolateThread = CurrentIsolate.getCurrentThread();
-        IsolateThread requestIsolate = Isolates.createIsolate(Isolates.CreateIsolateParameters.getDefault());
+        var currentIsolateThread = CurrentIsolate.getCurrentThread();
+
+        final IsolateThread requestIsolate = ms.getMeterRegistry().timer("isolate.create")
+                                               .record(this::doCreateIsolate);
 
         final ObjectHandle result;
+        Instant before = Instant.now();
+        final ObjectHandle classNameHandle = getRegistry().createHandle(requestIsolate, className);
+        final ObjectHandle methodNameHandle = getRegistry().createHandle(requestIsolate, methodName);
+        final ObjectHandle argsHandle = getRegistry().createHandle(requestIsolate, args);
+        Instant after = Instant.now();
+        ms.getMeterRegistry().timer("isolate.input_conversion").record(Duration.between(before, after));
 
-        result = FunctionRunnerImpl.execute(
-                requestIsolate,
-                currentIsolateThread,
-                getRegistry().createHandle(
-                        requestIsolate,
-                        className
-                ),
-                getRegistry().createHandle(
-                        requestIsolate,
-                        methodName
-                ),
-                getRegistry().createHandle(
-                        requestIsolate,
-                        args
-                ));
+        result = ms.getMeterRegistry().timer("isolate.execution").record(() -> FunctionRunnerImpl.execute(
+            requestIsolate,
+            currentIsolateThread,
+            classNameHandle,
+            methodNameHandle,
+            argsHandle));
 
+        ms.getMeterRegistry().timer("isolate.teardown").record(() -> doTearDownIsolate(requestIsolate));
+
+        return ms.getMeterRegistry().timer("isolate.output_conversion").record(() -> {
+            byte[] bytes = HandleUnwrapUtils.get(result);
+            return SerializationUtils.deserialize(bytes);
+        });
+    }
+
+    private IsolateThread doCreateIsolate() {
+        return Isolates.createIsolate(Isolates.CreateIsolateParameters.getDefault());
+    }
+
+    private void doTearDownIsolate(IsolateThread requestIsolate) {
         Isolates.tearDownIsolate(requestIsolate);
-        byte[] bytes = HandleUnwrapUtils.get(result);
-
-        return SerializationUtils.deserialize(bytes);
     }
 
     @CEntryPoint
