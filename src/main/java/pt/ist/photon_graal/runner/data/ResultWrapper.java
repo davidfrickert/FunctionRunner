ef1293db7af54bab879b04eefb143d9bce2eeb85
David Frickert <davidjfrickert@gmail.com>
2021-05-09 15:31:30 +0100

    send metrics in tuple

--- src/main/java/pt/ist/photon_graal/runner/data/ResultWrapper.java
+++ src/main/java/pt/ist/photon_graal/runner/data/ResultWrapper.java
@@ -1,26 +1,30 @@
 package pt.ist.photon_graal.runner.data;
 
+import io.vavr.Tuple2;
+import pt.ist.photon_graal.helpers.Tuple;
+
 import java.io.Serializable;
 import java.time.Duration;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ResultWrapper<T> implements Serializable {
     private static final long serialVersionUID = 42L;
 
-    private final Map<String, Duration> stats;
+    private final List<Tuple<String, Duration>> stats;
     private final T result;
 
-    public ResultWrapper(Map<String, Duration> stats, T result) {
+    public ResultWrapper(List<Tuple<String, Duration>> stats, T result) {
         this.stats = stats;
         this.result = result;
     }
 
     public ResultWrapper(T result) {
-        this(new HashMap<>(), result);
+        this(new LinkedList<>(), result);
     }
 
-    public Map<String, Duration> getStats() {
+    public List<Tuple<String, Duration>> getStats() {
         return stats;
     }
 
