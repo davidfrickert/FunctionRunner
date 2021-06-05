42a60378c955425f0867474144bd169c3c1942b7
dfrickert <david.frickert@premium-minds.com>
2021-06-05 15:53:25 +0100

    BaseJsonNode -> JsonNode

--- src/main/java/pt/ist/photon_graal/rest/api/DTOFunctionExecute.java
+++ src/main/java/pt/ist/photon_graal/rest/api/DTOFunctionExecute.java
@@ -1,22 +1,21 @@
 package pt.ist.photon_graal.rest.api;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.BaseJsonNode;
 import pt.ist.photon_graal.settings.Settings;
 import pt.ist.photon_graal.settings.CurrentSettings;
 
 public class DTOFunctionExecute {
     private final String classFQN;
     private final String methodName;
-    private final BaseJsonNode args;
+    private final JsonNode args;
 
     private final Boolean isStatic;
 
-    private DTOFunctionExecute(Settings functionSettings, BaseJsonNode args) {
+    private DTOFunctionExecute(Settings functionSettings, JsonNode args) {
         this(functionSettings.getClassFQN(), functionSettings.getMethodName(), functionSettings.isStatic(), args);
     }
 
-    private DTOFunctionExecute(String classFQN, String methodName, boolean isStatic, BaseJsonNode args) {
+    private DTOFunctionExecute(String classFQN, String methodName, boolean isStatic, JsonNode args) {
         this.classFQN = classFQN;
         this.methodName = methodName;
         this.args = args;
@@ -31,7 +30,7 @@ public class DTOFunctionExecute {
         return methodName;
     }
 
-    public BaseJsonNode getArgs() {
+    public JsonNode getArgs() {
         return args;
     }
 
@@ -46,15 +45,15 @@ public class DTOFunctionExecute {
         return isStatic != null && isStatic;
     }
 
-    public static DTOFunctionExecute of(Settings functionSettings, BaseJsonNode args) {
+    public static DTOFunctionExecute of(Settings functionSettings, JsonNode args) {
         return new DTOFunctionExecute(functionSettings, args);
     }
 
-    public static DTOFunctionExecute of(BaseJsonNode args) {
+    public static DTOFunctionExecute of(JsonNode args) {
         return new DTOFunctionExecute(CurrentSettings.VALUE, args);
     }
 
-    public static DTOFunctionExecute of(String classFQN, String methodName, boolean isStatic, BaseJsonNode args) {
+    public static DTOFunctionExecute of(String classFQN, String methodName, boolean isStatic, JsonNode args) {
         return new DTOFunctionExecute(classFQN, methodName, isStatic, args);
     }
 }
--- src/main/java/pt/ist/photon_graal/runner/FunctionRunner.java
+++ src/main/java/pt/ist/photon_graal/runner/FunctionRunner.java
@@ -1,9 +1,9 @@
 package pt.ist.photon_graal.runner;
 
-import com.fasterxml.jackson.databind.node.BaseJsonNode;
+import com.fasterxml.jackson.databind.JsonNode;
 import io.vavr.control.Either;
 import pt.ist.photon_graal.runner.isolateutils.IsolateError;
 
 public interface FunctionRunner {
-    <T> Either<IsolateError, T> run(String className, String methodName, BaseJsonNode Args);
+    <T> Either<IsolateError, T> run(String className, String methodName, JsonNode Args);
 }
